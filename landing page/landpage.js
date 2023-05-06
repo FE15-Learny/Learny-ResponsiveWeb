4de935652650580ce471811c78d9a53f414e3868
FadlyLugina <fadlylugina211@gmail.com>
2023-05-06 22:10:38 +0700

    revisian

--- landing page/landpage.js
+++ landing page/landpage.js
@@ -13,22 +13,21 @@ hamburgerMenu.addEventListener("click", () => {
 // ==================== BUTTON LOGIN / SIGN UP ============================
 
 // kode JavaScript untuk mengarahkan pengguna ke halaman login.html saat tombol Log In ditekan
-document.addEventListener("DOMContentLoaded", function() {
-    var logInButton = document.getElementById("login");
-  
-    logInButton.addEventListener("click", function() {
-      // mengarahkan pengguna ke halaman login.html
-      window.location.href = "login.html";
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var logInButton = document.getElementById("login");
+
+  logInButton.addEventListener("click", function () {
+    // mengarahkan pengguna ke halaman login.html
+    window.location.href = "../loginpage/login.html";
   });
+});
 
 //   kode JavaScript untuk mengarahkan pengguna ke halaman signup.html saat tombol Sign Up ditekan:
-document.addEventListener("DOMContentLoaded", function() {
-    var signUpButton = document.getElementById("signup");
-  
-    signUpButton.addEventListener("click", function() {
-      // mengarahkan pengguna ke halaman signup.html
-      window.location.href = "signup.html";
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var signUpButton = document.getElementById("signup");
+
+  signUpButton.addEventListener("click", function () {
+    // mengarahkan pengguna ke halaman signup.html
+    window.location.href = "../loginpage/signup.html";
   });
-  
+});
